3ba2513046673731d676d13dd80df999501e0a6e
Brian Pomerantz <bbp114@psu.edu>
2017-01-27 21:50:19 -0500

    through DFT

--- calc.py
+++ calc.py
@@ -16,15 +16,34 @@ L = float(sys.argv[1]);
 N = int(sys.argv[2]);
 ngal = int(sys.argv[3]);
 
+H = L/N;
+
 x = np.zeros(ngal);
 y = np.zeros(ngal);
 z = np.zeros(ngal);
+
+n = np.zeros((N, N, N));
+deltax = -1*np.ones((N, N, N), dtype=float);
+
+nbar = float(ngal)/(N*N*N);
+
+print(nbar);
 
 for i in range(ngal):
     x[i] = random.uniform(0, L);
     y[i] = random.uniform(0, L);
     z[i] = random.uniform(0, L);
+    
+    xi = int(round((N-1)*x[i]/L));
+    yi = int(round((N-1)*y[i]/L));
+    zi = int(round((N-1)*z[i]/L));
 
+    n[xi][yi][zi] += 1;
+    deltax[xi][yi][zi] += 1.0/nbar;
+
+deltak = abs(np.fft.rfftn(deltax))**2;
+
+print(deltak);
 
 #plt.plot(k, pktemp);
 #plt.xscale('log');
